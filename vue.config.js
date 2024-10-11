bbae27f96e96d7eac40138a8a3445afdb6387d77
Ederson Guilherme de Souza <70489050+git-osouza@users.noreply.github.com>
2024-10-11 15:36:20 -0300

    adjustments vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const { defineConfig } = require('@vue/cli-service');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'src/php'),
+            to: path.resolve(__dirname, 'dist/php')
+          }
+        ]
+      })
+    ]
+  }
+};
